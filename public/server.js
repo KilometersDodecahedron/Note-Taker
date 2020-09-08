7c7643bd2c6eaabdf5a29752ef81980b60c6d6b9
Miles Cohn <milesselim@icloud.com>
2020-09-08 15:28:25 -0400

    You can now save and delete notes, and they are then displayed dynamically

--- public/server.js
+++ public/server.js
@@ -9,14 +9,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('../public'))
 app.use(express.json());
 
+app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id;
+    
+    var noteArray = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+
+    noteArray = noteArray.filter((value) => { 
+        if(value.id != id){
+            return value;
+        }
+    })
+
+    var reformattedData = JSON.stringify(noteArray);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), reformattedData);
+
+    res.send("Success");
+});
+
 app.post("/api/notes", (req, res) => {
     var newNote = req.body;
 
+    //get the saved data
     var noteArray = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
     noteArray.push(newNote);
 
     var reformattedData = JSON.stringify(noteArray);
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), reformattedData);
+
+    res.send("Success");
 });
 
 app.get("/api/notes", (req, res) => {
